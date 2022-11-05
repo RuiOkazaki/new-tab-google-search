744ba184e543c6f50b0f94260543e6e2ad50fc18
RuiOkazaki <okazaki200226@gmail.com>
2022-11-06 00:20:33 +0900

    fix: bug fix

--- index.js
+++ index.js
@@ -3,18 +3,19 @@ const modeChangeButton = document.getElementById("modeChangeButton");
 const darkModeOn = () => {
   document.documentElement.classList.add("dark");
   modeChangeButton.checked = true;
-  sessionStorage.setItem("isOsDark", true);
+  chrome.storage.local.set({ isOsDarkState: true });
 };
 const darkModeOff = () => {
   document.documentElement.classList.remove("dark");
   modeChangeButton.checked = false;
-  sessionStorage.setItem("isOsDark", false);
+  chrome.storage.local.set({ isOsDarkState: false });
 };
 const listener = (e) => (e.matches ? darkModeOn() : darkModeOff());
 isOsDark.addEventListener("change", listener);
-window.addEventListener("load", () => {
-  if (sessionStorage.getItem("isOsDark") === undefined) listener(isOsDark);
-  if (Boolean(sessionStorage.getItem("isOsDark"))) {
+window.addEventListener("load", async () => {
+  const { isOsDarkState } = await chrome.storage.local.get();
+  if (isOsDarkState === undefined) listener(isOsDark);
+  if (isOsDarkState) {
     darkModeOn();
   } else {
     darkModeOff();
